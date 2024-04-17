c21b0aa4398f996bb71626f77bda6cbd97a94dd7
Eugene M. Reyes <eugenemreyes123@gmail.com>
2024-04-18 04:36:57 +0800

    Updated the assigned ai

--- cannine_care/main/views.py
+++ cannine_care/main/views.py
@@ -47,7 +47,7 @@ def home(request):
   context = {}
 
   # Model and Intents directory
-  chatbot_model_dir = str(base_dir) +"/main/chatbot-models/chatbot_2024-03-31_21-37-48.h5"
+  chatbot_model_dir = str(base_dir) +"/main/chatbot-models/chatbot_2024-04-17_20-29-22.h5"
   intents_dir = str(base_dir) +"/main/dataset/intents.json"
   model_data = chat.initialize_static_chatbot_requirements(chatbot_model_dir, intents_dir)
 
